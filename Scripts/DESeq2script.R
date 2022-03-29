45a4310d2b2dcb9f4b074ec87f624d0ad370d6ff
Desiree Wilson <desiree.s.wilson@gmail.com>
2022-03-29 16:30:31 -0500

    March 26, 2022 Okay, so far, the pipeline I created earlier worked. I am able to see the gene symbols now so cool. Now, finally, I will analyze the data using DESeq2.
    March 27, 2022
    LoLz so the IT research core killed my run because
    I did not use sbatch or srun to run my scripts. I
    totally forgot to do this. I will have to start
    this process all over but that is okay. :) Also,
    I finally finished creating the DESeq2 segment of
    the pipeline. I should see some results later
    tonight.

---
+++ Scripts/DESeq2script.R
@@ -0,0 +1,41 @@
+library(tximport)
+library(rhdf5)
+library(DESeq2)
+source("dirForDEanalysis.R")
+
+
+#read in full sample names:
+sampleNameFull <- read.csv(sampleNameFullFile, header = FALSE)
+
+#read in phenotype file:
+pheno <- read.csv(phenoDir, header = TRUE, sep="\t")
+
+#create sample files array:
+##remember, I lauched R in the same directory as this file:
+files <- file.path(sampleDir, sampleNameFull[,1], "abundance.tsv")
+names(files) <- pheno$sampleName
+
+#importing the abundance.tsv data:
+txi.tx <- tximport(files, type = "kallisto", txOut = TRUE)
+
+#creating the DESeq object for analysis
+dds <- DESeqDataSetFromTximport(txi.tx, pheno, ~sex)
+
+#filtering
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+
+#DE analysis
+dds <- DESeq(dds)
+res <- results(dds)
+
+#reordering matrix by pvalue from smallest to largest
+resOrdered <- res[order(res$pvalue),]
+
+#writing the results to file
+date <-format(Sys.time(), format = "%Y-%m-%d_%H_%M_%S")
+outputDir <- file.path(DESeqOutputDir, date)
+dir.create(outputDir)
+outputFile <- file.path(outputDir, "DESeq2-MaleVsFemale.txt")
+write.csv(resOrdered, outputFile)
+
